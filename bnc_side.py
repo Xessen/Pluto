0909477589b04b4e94e989d988edef903be99a6c
Xessen <seyhmusb@yandex.com>
2021-11-01 08:27:44 +0300

    Done,Probably

--- bnc_side.py
+++ bnc_side.py
@@ -1,10 +1,19 @@
-import numpy
-from decimal import *
-a=0
-while a<20:
-    print("")
-    for i in range(1,10):
-        print("b")
-        if i==4:
-            break
-    a+=1
+def get_signal(c_symbol):
+        handler= TA_Handler(symbol=c_symbol,exchange="binance",screener="crypto",interval="15m",timeout=None)
+        anal=handler.get_analysis()
+
+
+        OPENING_PRICE=anal.indicators['open']
+        EMA=anal.indicators['EMA200']
+        P_SAR=anal.indicators['P.SAR']
+
+
+
+        if self.prev_indi[c_symbol]>0 and OPENING_PRICE>P_SAR and P_SAR>EMA :
+            INDI_=P_SAR-OPENING_PRICE
+            self.prev_indi.update({c_symbol:INDI_})
+            return True,P_SAR,OPENING_PRICE
+        else:
+            INDI_=P_SAR-OPENING_PRICE
+            self.prev_indi.update({c_symbol:INDI_})
+            return False,False,False
--- test.py
+++ test.py
@@ -40,6 +40,9 @@ def get_oco_quantity(symbol):
                 break    
     else:
         return int(x)
+    if Decimal(x)>0:
+        return(x.split('.')[0])
+       
     return Decimal(x.split('.')[0]+'.'+x.split('.')[1][0:count+2])
 
 
@@ -53,15 +56,48 @@ def sl_limit(P_SAR,CURR_PRICE):
 def set_sl_limit(symbol,p_sar,curr_price):
     qty=get_oco_quantity(symbol=symbol)
     stop_loss,limit_p=sl_limit(p_sar,curr_price)
+    print(type(stop_loss))
+    print(type(limit_p))
 
     client.order_oco_sell(symbol=symbol,quantity=qty,price=limit_p,stopPrice=stop_loss,stopLimitPrice=stop_loss,stopLimitTimeInForce="GTC")
 
+prev_indi={'COCOSUSDT':1}
+
+def get_signal(c_symbol):
+        handler= TA_Handler(symbol=c_symbol,exchange="binance",screener="crypto",interval="15m",timeout=None)
+        anal=handler.get_analysis()
+
+
+        OPENING_PRICE=anal.indicators['open']
+        EMA=anal.indicators['EMA200']
+        P_SAR=anal.indicators['P.SAR']
+
+
+
+        if prev_indi[c_symbol]>0 and OPENING_PRICE>P_SAR and P_SAR>EMA :
+            INDI_=P_SAR-OPENING_PRICE
+            prev_indi.update({c_symbol:INDI_})
+            print(prev_indi)
+            return True,P_SAR,OPENING_PRICE
+        else:
+            INDI_=P_SAR-OPENING_PRICE
+            prev_indi.update({c_symbol:INDI_})
+            print(prev_indi)
+            return False,False,False
+
+        
+
+#print(get_signal('COCOSUSDT'))
+#print(prev_indi['COCOSUSDT']>0)
 #print(client.get_symbol_info("ALPACAUSDT"))
-#set_sl_limit("SHIBUSDT",0.00007172,0.00007589)
+#set_sl_limit("KNCUSDT",1.796,1.915)
 #print(float(0.99 * (float(0.0015))))
-#print(get_oco_quantity("SHIBUSDT"))
+print(get_oco_quantity("ZRXUSDT"))
+set_sl_limit("ZRXUSDT",1.3065,1.4462)
 #print(sl_limit(34765.74,34594.16))
-orders=client.get_open_orders()
+#orders=client.get_open_orders()
 
 
-print(orders)
+
+#print(orders)
+
